c87d4e0bc7bee15f61a9355feabcdcdf1dd28804
Keeyan Ghoreshi <keeyan.ghoreshi@uconn.edu>
2017-10-26 19:06:30 -0400

    Added forward algorithm

--- Bioinformatics/MarkovModels/HiddenMarkovModel.py
+++ Bioinformatics/MarkovModels/HiddenMarkovModel.py
@@ -1,16 +1,49 @@
 import numpy as np
 
 
+
 class HiddenMarkovModel():
     def __init__(self, states, end_state = "$"):
         self.states = states
         self.end_state = end_state
-        self.current_state = states[0]
+        self.simulation = []
     def simulate(self, max_iteration=100):
-        current_state = self.current_state
+        current_state = self.states[0]
         for i in range(max_iteration):
-            print(current_state.emit(include_state=True))
+            emission = current_state.emit(include_state=True)
+            print(emission)
+            self.simulation.append(emission)
             current_state = current_state.transition()
+    def forward(self, begin_state):
+        """
+        Does the forward algorithm on the simulation data
+        :param begin_state: The state of the first data point
+        :return: An array of the probabilities of a state at a certain time given the previous data
+        """
+        if len(self.simulation)==0:
+            print("No simulation was run")
+            return 0
+        path = [x[1] for x in self.simulation]
+        observations = [x[0] for x in self.simulation]
+        k = len(self.states)
+        L = len(observations)
+        # f[state, location]
+        f = np.zeros([k, L])
+        for state in range(k):
+            if state == begin_state:
+                f[state, 0] = 1
+            else:
+                f[state, 0] = 0
+        for i in range(1, L):
+            observation = observations[i]
+
+            for state in range(k):
+                summation = sum([f[s2,i-1]*self.states[s2].transition_dict[self.states[state]] for s2 in range(k)])
+                emission_prob = self.states[state].emission_dict[observation]
+                summation = summation*emission_prob
+                f[state, i] = summation
+        return f
+
 
 class State():
     def __init__(self, emission_dict, transition_dict=None, name="None"):
@@ -53,3 +86,4 @@ Loaded.set_transition(Loaded, .9)
 Loaded.set_transition(Fair,.1)
 HMM = HiddenMarkovModel([Fair,Loaded])
 HMM.simulate()
+print(HMM.forward(0))
